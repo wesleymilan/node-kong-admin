080771931e97c29b3e98b260af2a9d179ef9cd15
Wesley Milan <wesleybatista@gmail.com>
2019-05-08 15:14:33 -0300

    adding new services

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 'use strict';
 
 // Library extensions
+const CONFIG = require('./config');
+const NODE = require('./node');
+const TAG = require('./tag');
 const SERVICE = require('./service');
 const ROUTE = require('./route');
+const UPSTREAM = require('./upstream');
+const TARGET = require('./target');
 const CONSUMER = require('./consumer');
 const PLUGIN = require('./plugin');
 const CERTIFICATE = require('./certificate');
+const SNI = require('./sni');
 
 function Kong(params) {
 
@@ -13,11 +19,17 @@ function Kong(params) {
         url: params.url || 'https://localhost:8001'
     };
 
+    this.config = new CONFIG(PARAMS);
+    this.node = new NODE(PARAMS);
+    this.tag = new TAG(PARAMS);
     this.service = new SERVICE(PARAMS);
     this.route = new ROUTE(PARAMS);
+    this.upstream = new UPSTREAM(PARAMS);
+    this.target = new TARGET(PARAMS);
     this.consumer = new CONSUMER(PARAMS);
     this.plugin = new PLUGIN(PARAMS);
     this.certificate = new CERTIFICATE(PARAMS);
+    this.sni = new SNI(PARAMS);
 
 }
 
